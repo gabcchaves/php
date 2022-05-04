468857b4bbd3315944ca9306528631f27c5bcd6e
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-04 20:31:01 -0300

    Data types, to be continued.

---
+++ doc-study/reference/data_types/types.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Scalar types */
+$boolean = true;
+$string = "Some string";
+$string1 = "Some other string";
+$integer = 12;
+$float = 12.5;
+
+echo gettype($boolean) . PHP_EOL;
+echo gettype($string) . PHP_EOL;
+echo gettype($string1) . PHP_EOL;
+echo gettype($integer) . PHP_EOL;
+echo gettype($float) . PHP_EOL;
