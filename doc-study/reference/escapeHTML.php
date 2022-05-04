712d5c38401a48882b40c551b428edae680cab1f
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-04 20:04:57 -0300

    Escaping HTML.

---
+++ doc-study/reference/escapeHTML.php
@@ -0,0 +1,10 @@
+<p>This is a paragraph</p>
+<?php echo "Something"; ?>
+<p>This is another paragraph</p>
+
+<?php $condition = true; ?>
+<?php if ($condition): ?>
+	Displayed if true condition.
+<?php else: ?>
+	Displayed otherwise.
+<?php endif; ?>
