cf77ac5b0fca5f007995229ba5a26f53cce4ca37
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-05 16:50:35 -0300

    Basic types.

--- doc-study/reference/data_types/types.php
+++ doc-study/reference/data_types/types.php
@@ -1,14 +1,18 @@
 <?php
+    /* Scalar types */
+    $boolean = true;
+    $string = "Some string";
+    $string1 = "Some other string";
+    $integer = 12;
+    $float = 12.5;
 
-/* Scalar types */
-$boolean = true;
-$string = "Some string";
-$string1 = "Some other string";
-$integer = 12;
-$float = 12.5;
+	// Print the type of each variable
+    echo gettype($boolean) . PHP_EOL;
+    echo gettype($string) . PHP_EOL;
+    echo gettype($string1) . PHP_EOL;
+    echo gettype($integer) . PHP_EOL;
+    echo gettype($float) . PHP_EOL;
 
-echo gettype($boolean) . PHP_EOL;
-echo gettype($string) . PHP_EOL;
-echo gettype($string1) . PHP_EOL;
-echo gettype($integer) . PHP_EOL;
-echo gettype($float) . PHP_EOL;
+    // Numeric strings
+    $foo = 1 + '1.5';
+    echo $foo . PHP_EOL;
