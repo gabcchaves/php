4f598dedeb2573f82cdca724616c2e138cd1ed22
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-06 08:08:55 -0300

    $var = 'PHP Tutorial'. Put this variable into the title section, h3 tag and as an anchor text within an HTML document. Go to the editor Sample Output :
    PHP Tutorial
    PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.
    Go to the PHP Tutorial.

---
+++ w3resources/basic/e3.php
@@ -0,0 +1,5 @@
+<?php
+$var = 'PHP Tutorial';
+echo '<h1 id="title">' . $var . '</h1>';
+echo '<p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>';
+echo 'Go to the <a href="#title">' . $var . '</a>.';
