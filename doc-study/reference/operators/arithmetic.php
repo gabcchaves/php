5c698a580b070a6a60e509c5d423aa6eef864fe4
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-11 15:35:27 -0300

    Arithmetic operators.

---
+++ doc-study/reference/operators/arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+// Identity
+$a = false;
+echo +$a . PHP_EOL;
+
+// Negation
+$a = -10;
+echo -$a . PHP_EOL;
+
+// Addition
+$a = $b = 10;
+echo $a + $b . PHP_EOL;
+
+// Subtraction
+echo $a - $b . PHP_EOL;
+
+// Multiplication
+echo $a * $b . PHP_EOL;
+
+// Division
+echo $a / $b . PHP_EOL;
+
+// Modulus
+echo $a % $b . PHP_EOL;
+
+// Exponential
+echo $a**$b . PHP_EOL;
