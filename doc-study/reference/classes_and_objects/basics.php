67eb5c0903d78b7e1b6b99e23f4930378f341406
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-14 08:40:50 -0300

    Simple class and instantiation, invoking methods.

---
+++ doc-study/reference/classes_and_objects/basics.php
@@ -0,0 +1,62 @@
+<?php
+/* Simple class */
+class A {
+	private $prop;
+
+	function __construct($prop_value = 1) {
+		$this->prop = $prop_value;
+	}
+
+	public function get_prop() {
+		return $this->prop;
+	}
+
+	public function set_prop($prop_value) {
+		$this->prop = $prop_value;
+	}
+}
+
+// Instantiating class
+$a = new A();
+
+// Invoking object's methods
+$a->get_prop();
+$a->set_prop(2);
+
+var_dump($a->get_prop());
+
+/* Typed properties */
+class B {
+	private int $id;
+	private ?string $name;
+
+	public function __construct(int $id, ?string $name) {
+		// Typed properties need to
+		// be initialized before
+		// being accessed, otherwise
+		// and error is raised.
+		$this->id = $id;
+		$this->name = $name;
+	}
+
+	public function get_id() {
+		return $this->id;	
+	}
+
+	public function get_name() {
+		return $this->name;
+	}
+}
+
+$b = new B(1, 'Gabriel');
+var_dump($b->get_id());
+var_dump($b->get_name());
+
+/* Readonly properties */
+class C {
+	public readonly string $prop;
+	u
+}
+
+// EOF
+
