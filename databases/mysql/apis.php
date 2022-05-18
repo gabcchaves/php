efc9a3c070b7f052420ce492f6992c597fe8b667
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-17 22:17:20 -0300

    Closed mysqli connection.

--- databases/mysql/apis.php
+++ databases/mysql/apis.php
@@ -12,6 +12,9 @@ while ($row = $result->fetch_assoc()) {
 	print $row['name'] . PHP_EOL;
 }
 
+// Close mysqli connection.
+mysqli_close($conn1);
+
 print PHP_EOL;
 
 /* PDO */
@@ -22,6 +25,8 @@ $query = $conn2->query("SELECT name FROM api;");
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 	print $row['name'] . PHP_EOL;
 }
+
+// I read on web that PDO automatically closes connection.
 
 // EOF
 
